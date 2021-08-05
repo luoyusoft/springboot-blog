93c0b5ab7dc0104afcc7389d89586c51c7f9a182
jinhaoxun <jinhaoxun@zhuanzhuan.comssh-add>
2021-08-05 16:13:10 +0800

    1. 修复

--- src/main/java/com/jinhx/blog/common/config/MybatisPlusConfig.java
+++ src/main/java/com/jinhx/blog/common/config/MybatisPlusConfig.java
@@ -1,16 +1,17 @@
 package com.jinhx.blog.common.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * @description: MybatisPlus配置类
- * @author: luoyu
- * @date: 2020/2/13 上午11:34
- * @version: 1.0.0
+ * MybatisPlusConfig
+ *
+ * @author jinhx
+ * @since 2018-11-21
  */
 @EnableTransactionManagement
 @Configuration
@@ -31,16 +32,24 @@ public class MybatisPlusConfig {
     /**
      * 分页插件
      */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+//        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+//        paginationInterceptor.setOverflow(false);
+//        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+//        paginationInterceptor.setLimit(500);
+//        // 开启 count 的 join 优化,只针对部分 left join
+//        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+//        return paginationInterceptor;
+//    }
+
+    // 最新版
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
-        paginationInterceptor.setOverflow(false);
-        // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        paginationInterceptor.setLimit(500);
-        // 开启 count 的 join 优化,只针对部分 left join
-        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-        return paginationInterceptor;
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
     }
 
 }
