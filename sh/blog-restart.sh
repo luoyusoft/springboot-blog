bf89a224d87b077db6f02bc32c599be012797fe1
jinhaoxun <jinhaoxun@zhuanzhuan.comssh-add>
2021-08-05 14:10:18 +0800

    1. debug

--- sh/blog-restart.sh
+++ sh/blog-restart.sh
@@ -3,4 +3,4 @@ curl https://jinhx.cc/api/actuator/shutdown -X POST
 sleep 20
 docker rm blog
 docker rmi jinhx128/blog:latest
-docker run -dit --name blog -v /usr/local/docker/blog/log:/usr/local/project/blog/log -v /etc/localtime:/etc/localtime:ro -p 8800:8800 -p 9999:9999 -p 465:465 jinhx128/blog:latest
+docker run -dit --name blog -v /usr/local/docker/blog/log:/usr/local/project/blog/log -v /etc/localtime:/etc/localtime:ro -p 8800:8800 -p 9999:9999 -p 12138:12138 -p 465:465 jinhx128/blog:latest
