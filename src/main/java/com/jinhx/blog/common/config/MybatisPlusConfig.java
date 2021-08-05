0dde1301040327838586ea71bac2db202fa29ed4
jinhaoxun <jinhaoxun@zhuanzhuan.comssh-add>
2021-08-05 17:16:48 +0800

    1. 修复

--- src/main/java/com/jinhx/blog/common/config/MybatisPlusConfig.java
+++ src/main/java/com/jinhx/blog/common/config/MybatisPlusConfig.java
@@ -1,6 +1,5 @@
 package com.jinhx.blog.common.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -31,24 +30,18 @@ public class MybatisPlusConfig {
 
     /**
      * 分页插件
+     *
+     * @return MybatisPlusInterceptor
      */
-//    @Bean
-//    public PaginationInterceptor paginationInterceptor() {
-//        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-//        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
-//        paginationInterceptor.setOverflow(false);
-//        // 设置最大单页限制数量，默认 500 条，-1 不受限制
-//        paginationInterceptor.setLimit(500);
-//        // 开启 count 的 join 优化,只针对部分 left join
-//        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-//        return paginationInterceptor;
-//    }
-
-    // 最新版
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        paginationInnerInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 1000 条，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(1000L);
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
         return interceptor;
     }
 
